5725c18a3555ff0e19234d62d6a81c2c81089c9d
Skyler Giannetti <sjg5901@psu.edu>
2023-08-01 01:43:06 +0000

    LIbrary mini project complete

--- src/main/java/Application/Service/AuthorService.java
+++ src/main/java/Application/Service/AuthorService.java
@@ -40,7 +40,7 @@ public class AuthorService {
      * @return all authors
      */
     public List<Author> getAllAuthors() {
-        return null;
+        return authorDAO.getAllAuthors();
     }
     /**
      * TODO: Use the AuthorDAO to persist an author. The given Author will not have an id provided.
@@ -49,6 +49,6 @@ public class AuthorService {
      * @return The persisted author if the persistence is successful.
      */
     public Author addAuthor(Author author) {
-        return null;
+        return authorDAO.insertAuthor(author);
     }
 }
--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+        return bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +51,20 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
+        if ( bookDAO.getBookByIsbn(book.getIsbn()) == null ) {
+            bookDAO.insertBook(book);
+            return book;
+        }
+        
         return null;
     }
+
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        return bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
